06e69ab5cbd4d02bb6fe07ab1eec968d40d3da54
RodrigoLV9 <rodrigolv.975@gmail.com>
2024-12-10 15:55:16 -0300

    feat(Header/Controls): Create functionality to change logo light and dark mode

--- src/components/Header/Controls.tsx
+++ src/components/Header/Controls.tsx
@@ -1,17 +1,23 @@
 import React from 'react'
-/* import { MdSunny } from "react-icons/md"; */
+import { useState } from 'react';
+import { MdSunny } from "react-icons/md";
 import { FaMoon } from "react-icons/fa";
 
 const Controls :React.FC = () => {
+
+  const [mode,setMode]=useState<boolean>(true)
+  const handleMode=():void=>{
+    setMode(!mode)
+  }
   return (
     <section className='controls'>
         <select name="mode-theme" id="">
             <option value="español">Español</option>
             <option value="english">English</option>
         </select>
-        <div className="mode">
-            <FaMoon/>
-        </div>
+        <button className="mode" onClick={handleMode}>
+            {mode ? <FaMoon/> : <MdSunny/>}
+        </button>
     </section>
   )
 }
