b37779f319c7ce64548821564eb4bbe9741f231d
RodrigoLV9 <rodrigolv.975@gmail.com>
2024-12-12 15:53:27 -0300

    style(Info): Add basic style to Info component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Header from './components/Header/Header'
 import QRGenerator from './components/GeneratorQR/QRGenerator'
+import Info from './components/Info'
 function App() {
 
   return (
     <div className='app'>
       <Header/> 
       <QRGenerator/>
+      <Info/>
     </div>
   )
 }
