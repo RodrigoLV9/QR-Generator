7458de4e151941c0c7169247fae9305b3d7e1e44
RodrigoLV9 <rodrigolv.975@gmail.com>
2024-12-12 15:58:56 -0300

    feat(Footer): Create basic structure of Footer component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import Header from './components/Header/Header'
 import QRGenerator from './components/GeneratorQR/QRGenerator'
 import Info from './components/Info'
+import Footer from './components/Footer'
 function App() {
 
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header/> 
       <QRGenerator/>
       <Info/>
+      <Footer/>
     </div>
   )
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/Footer.css'
+const Footer:React.FC = () => {
+  return (
+    <footer className='footer'>
+        <p>Made by RodrigoLV</p>
+        <p>© 2024 QR Generator</p>
+        <p>Mi portfolio</p>
+    </footer>
+  )
+}
+export default Footer
